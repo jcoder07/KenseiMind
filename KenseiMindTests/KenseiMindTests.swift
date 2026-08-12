4ba39ac77238fdf2e8ed90ee7dec250216c10b4a
Miguel Duran <mduranx64@gmail.com>
2026-08-11 22:23:42 -0300

    Initial Commit
    Set up KenseiMind Xcode project with build config wired through a
    gitignored Config.xcconfig, gitignore for per-developer Xcode state,
    and Apache-2.0 LICENSE.

---
+++ KenseiMindTests/KenseiMindTests.swift
@@ -0,0 +1,19 @@
+//
+//  KenseiMindTests.swift
+//  KenseiMindTests
+//
+//  Created by Juan Fernandez on 11-08-26.
+//
+
+import Testing
+@testable import KenseiMind
+
+struct KenseiMindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
